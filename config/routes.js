a5b52c6f3c714cf20d936c60c57fa037d7cd0ecc
ju161bra <ju161bra@htwg-konstanz.de>
2023-01-25 14:22:18 +0100

    impressum hinzugefügt

--- config/routes.js
+++ config/routes.js
@@ -33,6 +33,7 @@ module.exports.routes = {
   'GET /account/password': { action: 'account/view-edit-password' },
   'GET /account/profile': { action: 'account/view-edit-profile' },
   'GET /dsgvo': { view: 'pages/dsgvo' },
+  'GET /impressum': { view: 'pages/impressum' },
 
 
   //  ╔╦╗╦╔═╗╔═╗  ╦═╗╔═╗╔╦╗╦╦═╗╔═╗╔═╗╔╦╗╔═╗   ┬   ╔╦╗╔═╗╦ ╦╔╗╔╦  ╔═╗╔═╗╔╦╗╔═╗
