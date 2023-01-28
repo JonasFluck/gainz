c4657b148f90adf3983782d3fe94009f40ae07dc
Jonas Fluck <jo161flu@htwg-konstanz.de>
2023-01-28 12:51:09 +0100

    Fixed fetch import in action

--- api/controllers/product-suche.js
+++ api/controllers/product-suche.js
@@ -25,9 +25,12 @@ module.exports = {
 
 
   fn: async function () {
+    //Fetch muss so imortiert werden da es sich bei actions um CommonJS handelt und sails andere importe von node modulen hier nicht toleriert
+    const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+    console.log(fetch)
     let param = this.req.param('search');
     let result = [];
-    await fetch(`${this.req.baseUrl}/api/v1/products?isActive=true`)
+      await fetch(`${this.req.baseUrl}/api/v1/products?isActive=true`)
       .then(response => response.json())
       .then(data=> {
         data.forEach(element =>{
@@ -36,7 +39,6 @@ module.exports = {
           }
         });
       })
-
       return {result};
       }
 
